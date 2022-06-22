0c0ef4bee3702f85d9eb507fb0058c519727d44a
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-06-22 02:43:55 -0300

    feat add route admin separated default route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,31 @@
 import { useRouter } from 'next/router'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { ShopListProvider } from '../hooks/ShopList'
+import { queryClient } from '../services/query-client'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   const { asPath } = useRouter()
   if (asPath.split(/\//)[1] === 'admin') {
-    return <Component {...pageProps} />
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    )
   }
   return (
-    <ShopListProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </ShopListProvider>
+    <QueryClientProvider client={queryClient}>
+      <ShopListProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ShopListProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
