fbf8633626cbaacb56255c7c9508f05e01a64a3b
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-06-22 02:47:17 -0300

    chore change domain image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.tcdn.com.br'],
+    domains: ['firebasestorage.googleapis.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   }
 }
